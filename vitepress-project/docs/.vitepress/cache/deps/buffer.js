4743bd6db7efb81ae9f90faac526af1d795ac199
Sukiyous <193663514@qq.com>
2025-05-14 17:51:22 +0800

    添加base配置以适配GitHub Pages

---
+++ vitepress-project/docs/.vitepress/cache/deps/buffer.js
@@ -0,0 +1,6 @@
+import {
+  require_buffer
+} from "./chunk-S63TUSBE.js";
+import "./chunk-BUSYA2B4.js";
+export default require_buffer();
+//# sourceMappingURL=buffer.js.map
---
+++ vitepress-project/docs/.vitepress/cache/deps/events.js
@@ -0,0 +1,6 @@
+import {
+  require_events
+} from "./chunk-HY2NTF3E.js";
+import "./chunk-BUSYA2B4.js";
+export default require_events();
+//# sourceMappingURL=events.js.map
